b32f3e8e31a7a9251e0874a6534e9cdae0fdc2b5
esoemad5 <esoemad5@gmail.com>
2018-09-07 07:07:11 -0500

    Added navBar.js to make the nav bar 'better'. Edited all pages to incorperate it.

---
+++ Website/navBar.js
@@ -0,0 +1,12 @@
+/*Code was originally:
+
+		<a href="index.html">&nbsp; Home &nbsp;</a>
+		<a href="personnel.html">&nbsp; Personnel &nbsp;</a>
+		<a href="apply.html">&nbsp; Apply Now! &nbsp;</a>
+		<a href="aperture.html">&nbsp; Aperture Science inc. &nbsp;</a>
+		
+I had to remove the line breaks and tabs, JavaScript didn't like them*/
+
+let navBar = '<a href="index.html">&nbsp; Home &nbsp;</a><a href="personnel.html">&nbsp; Personnel &nbsp;</a><a href="apply.html">&nbsp; Apply Now! &nbsp;</a><a href="aperture.html"> &nbsp; Aperture Science inc. &nbsp;</a>';
+document.write(navBar);
+document.close();
